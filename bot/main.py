1e1acde552c69e00e9332bb80c8d2043e20233ac
Janne Holopainen <janne.holopainen@reaktor.com>
2018-10-01 11:47:33 +0300

    Add comments about bot-class

--- bot/main.py
+++ bot/main.py
@@ -3,10 +3,16 @@ from pathlib import Path
 
 import sc2
 
+# Bots are created as classes and they need to have on_step method defined
 class MyBot(sc2.BotAI):
     with open(Path(__file__).parent / "../botinfo.json") as f:
         NAME = json.load(f)["name"]
 
+    # On_step method is invoked each game-tick and should not take more than
+    # 2 seconds to run, otherwise the bot will timeout and cannot receive new
+    # orders.
+    # It is important to note that on_step is asynchronous - meaning practices
+    # for asynchronous programming should be followed.
     async def on_step(self, iteration):
         if iteration == 0:
             await self.chat_send(f"Name: {self.NAME}")
