9da8d597f33e4bddd110962223ed9036abffb41f
Overmind Challenge <juha.paananen+overmind@reaktor.com>
2018-12-18 09:56:03 +0000

    Bot class name must not be changed

--- bot/main.py
+++ bot/main.py
@@ -3,7 +3,8 @@ from pathlib import Path
 
 import sc2
 
-# Bots are created as classes and they need to have on_step method defined
+# Bots are created as classes and they need to have on_step method defined.
+# Do not change the name of the class!
 class MyBot(sc2.BotAI):
     with open(Path(__file__).parent / "../botinfo.json") as f:
         NAME = json.load(f)["name"]
