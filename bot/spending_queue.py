ff87eee2567594fdf0636b7b387c672ed71f4798
Blodir <Amwrailth@gmail.com>
2019-01-24 20:14:04 +0200

    readjust greed

--- bot/spending_queue.py
+++ bot/spending_queue.py
@@ -36,9 +36,9 @@ class SpendingQueue():
             if self.scouting_manager.observed_enemy_units.exists:
                 closest_distance = self.scouting_manager.observed_enemy_units.closest_distance_to(self.bot.own_natural)
                 if closest_distance > 100:
-                    distance_multiplier = 0.7
-                elif closest_distance > 90:
                     distance_multiplier = 0.8
+                elif closest_distance > 90:
+                    distance_multiplier = 0.9
                 elif closest_distance < 20:
                     distance_multiplier = 1.2
 
